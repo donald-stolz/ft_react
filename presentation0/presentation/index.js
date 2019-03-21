9ecf2fcfbcc2f0e40b926e071120384f0df1fbfb
donald-stolz <donaldtstolz@gmail.com>
2019-03-21 07:44:38 -0700

    Refactoring; Begin setup for presentation

---
+++ presentation0/presentation/index.js
@@ -0,0 +1,117 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  Image,
+  List,
+  ListItem,
+  Notes,
+  Quote,
+  Slide,
+  Text
+} from "spectacle";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+const images = {
+  formidagon: require("../assets/formidable-logo.svg"),
+  goodWork: require("../assets/good-work.gif")
+};
+
+// Require CSS
+require("normalize.css");
+
+const theme = createTheme(
+  {
+    primary: "white",
+    secondary: "#1F2022",
+    tertiary: "#03A9FC",
+    quaternary: "#CECECE"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck
+        transition={["zoom", "slide"]}
+        transitionDuration={500}
+        theme={theme}
+      >
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit lineHeight={1} textColor="secondary">
+            ft_react
+          </Heading>
+          <Heading size={3} margin="10px 0 0" textColor="tertiary">
+            1st Meeting
+          </Heading>
+        </Slide>
+        <Slide bgColor="secondary">
+          <Heading size={6} textColor="secondary" caps>
+            Outline
+          </Heading>
+          <List>
+            <ListItem>Introductions</ListItem>
+            <ListItem>Pre-reqs</ListItem>
+            <ListItem>Why React?</ListItem>
+            <ListItem>Demo</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>
+            Typography
+          </Heading>
+          <Heading size={1} textColor="secondary">
+            Heading 1
+          </Heading>
+          <Heading size={2} textColor="secondary">
+            Heading 2
+          </Heading>
+          <Heading size={3} textColor="secondary">
+            Heading 3
+          </Heading>
+          <Heading size={4} textColor="secondary">
+            Heading 4
+          </Heading>
+          <Heading size={5} textColor="secondary">
+            Heading 5
+          </Heading>
+          <Text size={6} textColor="secondary">
+            Standard text
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Standard List
+          </Heading>
+          <List>
+            <ListItem>Item 1</ListItem>
+            <ListItem>Item 2</ListItem>
+            <ListItem>Item 3</ListItem>
+            <ListItem>Item 4</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Example Quote</Quote>
+            <Cite margin="10px 0 0 30px">Author</Cite>
+          </BlockQuote>
+        </Slide>
+        <Slide>
+          <Image src={images.goodWork} width={500} />
+          <Notes>gifs work too</Notes>
+        </Slide>
+      </Deck>
+    );
+  }
+}
