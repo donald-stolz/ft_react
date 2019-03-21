642b7816c111cd3a1a4bc2f2bae6a2feaee8a039
donald-stolz <donaldtstolz@gmail.com>
2019-03-21 09:19:25 -0700

    Update presentation

--- presentation0/presentation/index.js
+++ presentation0/presentation/index.js
@@ -1,5 +1,5 @@
 // Import React
-import React from "react";
+import React from 'react';
 
 // Import Spectacle Core tags
 import {
@@ -14,29 +14,30 @@ import {
   Quote,
   Slide,
   Text
-} from "spectacle";
+} from 'spectacle';
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default';
 
 const images = {
-  formidagon: require("../assets/formidable-logo.svg"),
-  goodWork: require("../assets/good-work.gif")
+  formidagon: require('../assets/formidable-logo.svg'),
+  goodWork: require('../assets/good-work.gif')
 };
 
 // Require CSS
-require("normalize.css");
+require('normalize.css');
+require('./index.css');
 
 const theme = createTheme(
   {
-    primary: "white",
-    secondary: "#1F2022",
-    tertiary: "#03A9FC",
-    quaternary: "#CECECE"
+    primary: 'white',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quaternary: '#CECECE'
   },
   {
-    primary: "Montserrat",
-    secondary: "Helvetica"
+    primary: 'Montserrat',
+    secondary: 'Helvetica'
   }
 );
 
@@ -44,11 +45,11 @@ export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={["zoom", "slide"]}
+        transition={['zoom', 'slide']}
         transitionDuration={500}
         theme={theme}
       >
-        <Slide transition={["zoom"]} bgColor="primary">
+        <Slide transition={['zoom']} bgColor="primary">
           <Heading size={1} fit lineHeight={1} textColor="secondary">
             ft_react
           </Heading>
@@ -57,51 +58,48 @@ export default class Presentation extends React.Component {
           </Heading>
         </Slide>
         <Slide bgColor="secondary">
-          <Heading size={6} textColor="secondary" caps>
+          <Heading size={3} textColor="tertiary" caps>
             Outline
           </Heading>
-          <List>
+          <List textColor="tertiary">
             <ListItem>Introductions</ListItem>
             <ListItem>Pre-reqs</ListItem>
             <ListItem>Why React?</ListItem>
             <ListItem>Demo</ListItem>
           </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={5} textColor="primary" caps>
+            Pre-reqs to learning React
           </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
-        </Slide>
-        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+          <List textColor="primary">
+            <ListItem>JavaScript</ListItem>
+            <ListItem>HTML & CSS</ListItem>
           </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={4} textColor="secondary">
+            What is React?
+          </Heading>
+          <Text style={{ marginBottom: '50px' }} textColor="tertiary">
+            React is a JavaScript library for building user interfaces
+          </Text>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="primary">
+          <Heading size={4} textColor="secondary">
+            Why should you learn it?
+          </Heading>
+          <List textColor="tertiary">
+            <ListItem>Reusable & Stateful Components</ListItem>
+            <ListItem>MVC</ListItem>
+            <ListItem>All JavaScript</ListItem>
+            <ListItem>Created & Mantained by FB</ListItem>
+            <ListItem>Learn once, write anywhere</ListItem>
+          </List>
+        </Slide>
+
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
           <BlockQuote>
             <Quote>Example Quote</Quote>
             <Cite margin="10px 0 0 30px">Author</Cite>
