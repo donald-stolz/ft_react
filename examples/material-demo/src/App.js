04e6b7e3e27e897343aa73a17307a2778f421c66
Donald Stolz <donaldtstolz@gmail.com>
2019-04-04 16:13:29 -0700

    Update demo

---
+++ examples/material-demo/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import withRoot from './withRoot';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+import withRoot from './withRoot';
+import 'typeface-roboto';
+
+const App = () => {
+  return (
+    <body>
+      <HeroImage />
+    </body>
+  );
+};
+
+App.propT;
+
+export default withRoot(App);
